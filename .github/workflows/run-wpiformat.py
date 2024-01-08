22f61d91cecc2791ebb3607d93c93a76a9ad428f
Drew Williams <williams.r.drew@gmail.com>
2024-01-08 10:31:49 -0500

    precommit check

---
+++ .github/workflows/run-wpiformat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+
+def main():
+    if sys.platform == 'win32':
+        cmd = (r'./venv/Scripts/wpiformat.exe')
+    else:
+        cmd = (r'./venv/bin/wpiformat')
+    return subprocess.call(cmd)
+
+if __name__ == '__main__':
+    exit(main())
